790aa271091fdad08ced83c612b787f3d019f657
guysutton <g.sutton@ru.ac.za>
2020-10-29 15:14:36 +0200

    Add SAC part #2 tut

--- functions/sp_accum_group_functions.R
+++ functions/sp_accum_group_functions.R
@@ -57,3 +57,194 @@ plot_sp_accum_group <- function(x){
 # Test plotting function
 test_plot_fun <- plot_sp_accum_group(x = test_sac) 
 test_plot_fun 
+
+
+
+
+# Define function to compute all richness estimators (no groups)
+sp_est_all <- function(x){
+  
+  {{ x }} %>%
+    tidyr::nest(data = everything()) %>%
+    dplyr::mutate(data = purrr::map(data, vegan::poolaccum)) %>%
+    # Extract the observed species richness estimator (denoted by S)
+    dplyr::mutate(data_s    = purrr::map(data,
+                                       ~ data.frame(summary(.)$S,
+                                                    check.names = FALSE))) %>%
+    # Extract the chao richness estimator (denoted by chao)
+    dplyr::mutate(data_chao = purrr::map(data,
+                                       ~ data.frame(summary(.)$chao,
+                                                    check.names = FALSE))) %>%
+  # Extract the jack1 richness estimator (denoted by chao)
+  dplyr::mutate(data_jack = purrr::map(data,
+                                       ~ data.frame(summary(.)$jack1,
+                                                    check.names = FALSE))) %>%
+  # Extract the boot richness estimator (denoted by chao)
+  dplyr::mutate(data_boot = purrr::map(data,
+                                       ~ data.frame(summary(.)$boot,
+                                                    check.names = FALSE))) %>%
+  # Drop unnecessary columns
+  dplyr::select(-c(data)) %>%
+  # Make longer
+  tidyr::pivot_longer(
+    cols = data_s:data_boot,
+    names_to = "estimator"
+  ) %>%
+  tidyr::unnest(cols = c(value)) %>%
+  tidyr::unite(col = "value", 
+               c(S, Chao), 
+               sep = " ", 
+               na.rm = TRUE, 
+               remove = FALSE) %>%
+  dplyr::rename(jack1 = 9) %>%
+  tidyr::unite(col = "value", 
+               c(value, jack1), 
+               sep = " ", 
+               na.rm = TRUE, 
+               remove = FALSE) %>%
+  tidyr::unite(col = "value", 
+               c(value, Bootstrap), 
+               sep = " ", 
+               na.rm = TRUE, 
+               remove = FALSE) %>%
+  dplyr::mutate(value = as.numeric(value)) %>%
+  # Rename the columns
+  dplyr::rename(N = N,
+                S = S,
+                lower_ci = `2.5%`,
+                upper_ci = `97.5%`,
+                std_dev = Std.Dev) %>%
+  # Drop some rubbish columns
+  dplyr::select(-c(Chao, jack1, Bootstrap, std_dev, S)) %>%
+  dplyr::mutate(estimator = dplyr::case_when(
+    estimator == "data_s" ~ "S",
+    estimator == "data_chao" ~ "Chao",
+    estimator == "data_jack" ~ "Jacknife",
+    estimator == "data_boot" ~ "Bootstrap"
+  )) %>%
+  dplyr::mutate(estimator = as.factor(estimator)) %>%
+  dplyr::mutate(estimator = fct_relevel(estimator, 
+                                        "S",
+                                        "Chao",
+                                        "Jacknife",
+                                        "Bootstrap"))
+  
+}
+
+# Define function to plot the species richness estimators (no groups )
+plot_sp_est_all <- function(x){
+  
+  {{ x }} %>%
+    ggplot(data = ., aes(x = N,
+                         y = value)) +
+    geom_ribbon(aes(ymin = lower_ci,
+                    ymax = upper_ci),
+                alpha = 0.3) +
+    geom_line() +
+    labs(x = "No. of surveys",
+         y = "Estimated species richness") +
+    # Different panels for the different estimators
+    facet_wrap(~ estimator, ncol = 4)
+
+}
+
+b <- plot_sp_est_all(a)
+b
+
+# Define function to compute all richness estimators (no groups)
+
+sp_est_group <- function(x, groups){
+  
+  {{ x }} %>%
+    dplyr::group_by({{ groups }}) %>%
+    tidyr::nest() %>%
+    dplyr::mutate(data = purrr::map(data, vegan::poolaccum)) %>%
+    # Extract the observed species richness estimator (denoted by S)
+    dplyr::mutate(data_s    = purrr::map(data,
+                                         ~ data.frame(summary(.)$S,
+                                                      check.names = FALSE))) %>%
+    # Extract the chao richness estimator (denoted by chao)
+    dplyr::mutate(data_chao = purrr::map(data,
+                                         ~ data.frame(summary(.)$chao,
+                                                      check.names = FALSE))) %>%
+    # Extract the jack1 richness estimator (denoted by chao)
+    dplyr::mutate(data_jack = purrr::map(data,
+                                         ~ data.frame(summary(.)$jack1,
+                                                      check.names = FALSE))) %>%
+    # Extract the boot richness estimator (denoted by chao)
+    dplyr::mutate(data_boot = purrr::map(data,
+                                         ~ data.frame(summary(.)$boot,
+                                                      check.names = FALSE))) %>%
+    # Drop unnecessary columns
+    dplyr::select(-c(data)) %>%
+    # Make longer
+    tidyr::pivot_longer(
+      cols = data_s:data_boot,
+      names_to = "estimator"
+    ) %>%
+    tidyr::unnest(cols = c(value)) %>%
+    tidyr::unite(col = "value", 
+                 c(S, Chao), 
+                 sep = " ", 
+                 na.rm = TRUE, 
+                 remove = FALSE) %>%
+    dplyr::rename(jack1 = 10) %>%
+    tidyr::unite(col = "value", 
+                 c(value, jack1), 
+                 sep = " ", 
+                 na.rm = TRUE, 
+                 remove = FALSE) %>%
+    tidyr::unite(col = "value", 
+                 c(value, Bootstrap), 
+                 sep = " ", 
+                 na.rm = TRUE, 
+                 remove = FALSE) %>%
+    dplyr::mutate(value = stringr::str_extract(value, "[^ ]+")) %>%
+    dplyr::mutate(value = as.numeric(value)) %>%
+    # Rename the columns
+    dplyr::rename(group = {{ groups }},
+                  N = N,
+                  S = S,
+                  lower_ci = `2.5%`,
+                  upper_ci = `97.5%`,
+                  std_dev = Std.Dev) %>%
+    # Drop some rubbish columns
+    dplyr::select(-c(Chao, jack1, Bootstrap, std_dev, S)) %>%
+    dplyr::mutate(estimator = dplyr::case_when(
+      estimator == "data_s" ~ "S",
+      estimator == "data_chao" ~ "Chao",
+      estimator == "data_jack" ~ "Jacknife",
+      estimator == "data_boot" ~ "Bootstrap"
+    )) %>%
+    dplyr::mutate(estimator = as.factor(estimator),
+                  group = as.factor(group)) %>%
+    dplyr::mutate(estimator = fct_relevel(estimator, 
+                                          "S",
+                                          "Chao",
+                                          "Jacknife",
+                                          "Bootstrap"))
+}
+
+# Define function to plot the species richness estimators (no groups )
+plot_sp_est_group <- function(x){
+  
+  {{ x }} %>%
+    ggplot(data = ., aes(x = N,
+                         y = value,
+                         group = group)) +
+    geom_ribbon(aes(ymin = lower_ci,
+                    ymax = upper_ci,
+                    fill = group),
+                alpha = 0.3) +
+    geom_line(aes(colour = group)) +
+    labs(x = "No. of surveys",
+         y = "Estimated species richness",
+         fill = "group") +
+    guides(colour = FALSE) + 
+    # Different panels for the different estimators
+    #facet_wrap(~ group + estimator, ncol = 4)
+    facet_grid(vars(group), vars(estimator)) +
+    theme(strip.text.y = element_blank(),
+          legend.position = "right")
+  
+}
---
+++ scripts/tut_17_species_accumulation_curves_part_2.R
@@ -0,0 +1,183 @@
+###########################################################################
+###########################################################################
+###########################################################################
+# Computing and plotting species accumulation curves in R -----------------
+###########################################################################
+###########################################################################
+###########################################################################
+
+# Today, we are going to look at how to compute and plot species 
+# accumulation curves in R, using the amazing 'vegan' package. 
+# In this session, we are going to cover the most basic SAC - observed richness.
+# - Observed species richness (hereafter 'S') simply tells us:
+#   (1) how many species we have recorded, to date, and 
+#   (2) whether more surveys could yield new additional species. 
+# - It DOES NOT tell us how many species could be in the community 
+#   (i.e. we CANNOT extrapolate species richness estimates from S).
+#   - Next week, we will cover how to extrapolate species richness 
+#     (part 2). 
+
+###########################################################################
+# Load packages -----------------------------------------------------------
+###########################################################################
+
+# Load packages 
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, 
+               tidyr, 
+               janitor,
+               vegan)
+
+###########################################################################
+# Set global defaults -----------------------------------------------------
+###########################################################################
+
+# Set ggplot theme (makes nice plots)
+theme_set(theme_classic() +
+            theme(panel.border = element_rect(colour = "black", fill = NA),
+                  axis.text = element_text(colour = "black"),
+                  axis.title.x = element_text(margin = unit(c(2, 0, 0, 0), "mm")),
+                  axis.title.y = element_text(margin = unit(c(0, 4, 0, 0), "mm")),
+                  legend.position = "none"))
+
+###########################################################################
+# Load and clean data -----------------------------------------------------
+###########################################################################
+
+# Load data 
+data_raw <- readxl::read_xlsx("./data_raw/tut_16_lycium_comm_data.xlsx")
+head(data_raw)
+
+# Clean data 
+data_clean <- data_raw %>%
+  janitor::clean_names() %>%
+  dplyr::mutate(season = if_else(season == 1, "Summer", "Winter"))
+head(data_clean)
+
+###########################################################################
+# Running SAC -------------------------------------------------------------
+###########################################################################
+
+# To run SAC, we need to only keep the columns with species abundances. 
+data_sac <- data_clean %>%
+  dplyr::select(-c(provinces, climatic_zones, site, season, haplotype))
+data_sac <- as.data.frame(data_sac)
+
+###########################################################################
+# Option #1: All samples (no groups) --------------------------------------
+###########################################################################
+
+# For example, this data represents surveys for insects across RSA on a 
+# shrub, Lycium ferocissimum, a biocontrol target in Australia. 
+# - Last week, we saw that the 56 surveys performed so far were NOT enough
+#   to record the full insect community on these plants.
+# - Let's say we now want to estimate how many more insects we could find
+#   if we did more surveys? 
+
+# Run SAC
+# - We calculated all the estimators last week, just didn't use them. 
+# - We feed in a data frame or matrix of a species abundance matrix
+#   - We must remove all the columns with site/province/ect data...
+class(data_sac)
+sac1 <- vegan::poolaccum(data_sac)
+plot(sac1)
+
+# This is literally it. 
+# - Below, we will extract the data so that we can plot it nicely. 
+
+# Extract chao
+chao <- data.frame(summary(sac1)$chao, check.names = FALSE)
+colnames(chao) <- c("N", "S", "lower2.5", "higher97.5", "std")
+head(chao)
+
+# Plot graph of chao estimated richness +- 95% CI
+# - We want our trend line to reach an asymptote (flat horizontal line)
+# - This would indicate that the rate at which we accumulate insects 
+#   with each additional survey has approximately become 0. 
+chao %>%
+  ggplot(data = ., aes(x = N,
+                       y = S)) +
+  # Add confidence intervals
+  geom_ribbon(aes(ymin = lower2.5,
+                  ymax = higher97.5),
+              alpha = 0.3) +
+  geom_line() +
+  labs(x = "No. of surveys",
+       y = "Species richness",
+       subtitle = "More surveys are required to find all the insects on this plant")
+
+# Not very helpful so far, right? 
+# - Estimators are only really relevant once we compare how our observed
+#   richness compares with estimates. 
+
+# We could plot observed richness on the same graph, but this doesn't
+# really do it for me personally. 
+
+# Extract S
+obs <- data.frame(summary(sac1)$S, check.names = FALSE)
+colnames(obs) <- c("N", "S", "lower2.5", "higher97.5", "std")
+head(obs)
+
+# Now make the plot
+chao %>%
+  ggplot(data = ., aes(x = N,
+                       y = S)) +
+  # Add confidence intervals
+  geom_ribbon(aes(ymin = lower2.5,
+                  ymax = higher97.5),
+              alpha = 0.3) +
+  geom_line() +
+  # Add S richness
+  geom_line(data= obs, aes(x = N,
+                           S),
+            linetype = "dashed") +
+  labs(x = "No. of surveys",
+       y = "Species richness",
+       subtitle = "Observed richness (dashed line) is much lower than expected - more surveys are required")
+
+###########################################################################
+# Option #2: Plot a couple different estimators + S
+###########################################################################
+
+# The code for this is a bit long and finicky, so I have written another
+# set of functions which will take care of business for us in the background. 
+
+# Load my custom functions 
+source("./functions/sp_accum_group_functions.r")
+
+# Let's use the function to compute all richness estimators (no groups)
+# - We have to specify:
+#   (1) x = the data frame containing the species-abundance 
+#           matrix and column identifiers (e.g. site, month, ect...).
+test_est <- sp_est_all(x = data_clean)
+test_est
+
+# Now, let's plot the different richness estimators 
+# - We have to specify:
+#   (1) x = which is now the variable containing the richness estimators 
+#           from above. 
+test_plot_fun <- plot_sp_est_all(x = test_est) 
+test_plot_fun 
+
+###########################################################################
+# Option #2: All samples (by group) --------------------------------------
+###########################################################################
+
+# What if we wanted to compute and plot the richness estimators for different
+# groups, like we did last week for summer vs winter and Eastern Cape vs Western Cape?
+
+# Compute the estimators by season: 
+
+# Let's use the function to compute all richness estimators (by season groups)
+# - We have to specify:
+#   (1) x = the data frame containing the species-abundance 
+#           matrix and column identifiers (e.g. site, month, ect...).
+aaa <- sp_est_group(x = data_clean, groups = season)  
+
+# Now, let's plot the different richness estimators 
+# - We have to specify:
+#   (1) x = which is now the variable containing the richness estimators 
+#           from above. 
+plot_sp_est_group(x = aaa) +
+  labs(fill = "Season") +
+  scale_x_continuous(limits = c(0, 30))
